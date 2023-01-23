4ca47b8496f4fad3508eb0e934f260f9116b4ad9
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-01-23 17:47:39 +0330

    add apiSlice

---
+++ src/app/api/apiSlice.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { logIn, logOut } from "../../features/auth/authSlice";
+import { BASE_URL, REFRESH_TOKEN_URL } from "./constants";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.originalStatus === 403) {
+    console.log("sending refresh token");
+    // send refresh token to get new access token
+    const refreshResult = await baseQuery(REFRESH_TOKEN_URL, api, extraOptions);
+    console.log(refreshResult);
+    if (refreshResult?.data) {
+      const user = api.getState().auth.user;
+      // store the new token
+      api.dispatch(logIn({ ...refreshResult.data, user }));
+      // retry the original query with new access token
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logOut());
+    }
+  }
+
+  return result;
+};
+
+export const apiSlice = createApi({
+  baseQuery: baseQueryWithReauth,
+  endpoints: (builder) => ({}),
+});
