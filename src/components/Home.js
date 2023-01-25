4c10e09c28a0c1ee780070c8e826ac93b885bee2
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-01-25 10:29:17 +0330

    add link to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Link to="/products">Products</Link>
+      <h1>Home</h1>
+    </>
+  );
 };
 
 export default Home;
